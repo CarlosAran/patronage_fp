cea619c7c616b37592d222ad8680f0d948b46e55
Jockxtar <carlos.aran.tapia@gmail.com>
2025-01-04 13:29:27 +0100

    *(FIX) Añadir EventoBean a la BDD.

--- app/src/main/java/com/patronage/patronage/data/AppDB.kt
+++ app/src/main/java/com/patronage/patronage/data/AppDB.kt
@@ -2,7 +2,7 @@ package com.patronage.patronage.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [PreguntaBean::class], version = 1)
+@Database(entities = [PreguntaBean::class, EventoBean::class], version = 2)
 abstract class AppDB : RoomDatabase() {
     abstract fun preguntaDao(): PreguntaDao
     abstract fun eventoDao(): EventoDao
